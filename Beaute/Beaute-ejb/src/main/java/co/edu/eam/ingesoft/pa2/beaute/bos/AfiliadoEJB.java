d724f2eff55ac1a65b63025c106d6de0e6c67e6c
Jairo Andres <jairo199316@hotmail.com>
2016-11-08 14:12:34 -0500

    Primer commit

--- Beaute/Beaute-ejb/src/main/java/co/edu/eam/ingesoft/pa2/beaute/bos/AfiliadoEJB.java
+++ Beaute/Beaute-ejb/src/main/java/co/edu/eam/ingesoft/pa2/beaute/bos/AfiliadoEJB.java
@@ -1,10 +1,5 @@
 package co.edu.eam.ingesoft.pa2.beaute.bos;
 
-
-
-import java.sql.SQLException;
-
-
 import javax.ejb.LocalBean;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
@@ -15,7 +10,7 @@ import co.edu.eam.ingesoft.pa2.beaute.implementacion.EJBGenerico;
 
 @LocalBean
 @Stateless
-public class AfiliadoEJB  extends EJBGenerico<Afiliado> {
+public class AfiliadoEJB extends EJBGenerico<Afiliado> {
 
 	@Override
 	public Class getClase() {
@@ -27,9 +22,9 @@ public class AfiliadoEJB  extends EJBGenerico<Afiliado> {
 		dao.crear(afiliado);
 
 	}
-	
-	public Afiliado buscar(Object pk){
+
+	public Afiliado buscar(Object pk) {
 		return dao.buscar(pk);
 	}
-	
+
 }
